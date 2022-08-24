805e73d7b94e1c61e812cdb9d35637d4e3ddf56a
srisarvesh r <r.srisarvesh@gmail.com>
2022-08-24 22:49:07 +0530

    After scheduling a cron job that gets data from the binance api

---
+++ utils/updationClient.js
@@ -0,0 +1,37 @@
+module.exports=(io)=>{
+    
+    const API_LINK=require("../configs/server.config").API_LINK;//This has the api link of the binance
+    const cron = require("node-cron");
+    const Client=require("node-rest-client").Client;//so we are requiring the Client object from the node-rest-client
+    const client=new Client();//new object of this Client
+    
+    
+    
+    io.on('connection',(socket)=>{
+         console.log("User is connected");
+
+         //so for every 5 seconds we are making a get call to the binance api and get the real time data updates.
+     cron.schedule("*/5 * * * * *",()=>{
+
+    try{
+
+      client.get(API_LINK,(data,response)=>{
+          //data is parsed js object and response is raw object
+          console.log(data);
+         socket.emit("showUpdatedData",data);//so when ever the data we retrived from that binance api for every 5 seconds we try to emit an event.
+      })
+    }
+    catch(err){
+       console.log(err.message);
+    }
+   })
+
+        
+         socket.on('disconnect',()=>{
+             console.log("User is disconnected");
+         });
+    });
+
+}
+
+
