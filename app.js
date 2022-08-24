dc9da8cec358151a04ffd59b8d6c899dc83f62a3
srisarvesh r <r.srisarvesh@gmail.com>
2022-08-24 22:54:41 +0530

    After adding app.js which is the entry point of the server

---
+++ app.js
@@ -0,0 +1,21 @@
+const  express = require('express');
+const app=express();
+const bodyParser=require('body-parser');
+const http = require('http').Server(app);
+const io=require('socket.io')(http);
+
+const serverConfig = require("./configs/server.config");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+
+
+
+
+
+require("./routes/app.routes")(app);
+require("./utils/updationClient")(io);
+
+
+http.listen(serverConfig.PORT,()=>{
+    console.log('listening on port '+serverConfig.PORT);
+})
